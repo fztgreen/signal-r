560b06dda9ad00d8006cebb9fcc74829d4e3ad97
Zachary Greenfield <Zachary.Greenfield@veteransunited.com>
2022-11-25 14:37:01 -0600

    initial commit

---
+++ signal-r.client/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:7041/ChatHub")
+    .Build();
+
+connection.Closed += async (error) =>
+{
+    await Task.Delay(new Random().Next(0, 5) * 1000);
+    await connection.StartAsync();
+};
+
+connection.On<string>("SendMessage", Console.WriteLine);
+
+connection.StartAsync();
+
+Console.WriteLine("Awaiting input...");
+Console.ReadLine();
---
+++ signal-r/Controllers/ChatHubController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using signal_r.Hubs;
+
+namespace signal_r.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatHubController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _context;
+
+        public ChatHubController(IHubContext<ChatHub> context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "SendMessage")]
+        public async Task<string> SendMessage(string message)
+        {
+            await _context.Clients.All.SendAsync("SendMessage", message);
+            return "Ok";
+        }
+    }
+}
